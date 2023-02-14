294f93f850b8c32af505bb1ab9e1071c398a4cfb
anilsulekoglu <aanil1201@gmail.com>
2023-02-14 22:40:39 +0300

    Model mapper eklendi

--- diary/src/main/java/com/gunce/diary/DiaryApplication.java
+++ diary/src/main/java/com/gunce/diary/DiaryApplication.java
@@ -1,13 +1,23 @@
 package com.gunce.diary;
 
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DiaryApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DiaryApplication.class, args);
+	}
+
+	@Bean
+	public ModelMapper getModelMapper() {
+		ModelMapper modelMapper = new ModelMapper();
+		modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+		return modelMapper;
 	}
 
 }
